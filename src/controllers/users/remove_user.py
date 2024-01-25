0e4a7b6c936fa252c31c3448171b63fb9991c530
tanmayjobs <tanmayjobs@gmail.com>
2024-01-25 16:18:27 +0530

    almost

---
+++ src/controllers/users/remove_user.py
@@ -0,0 +1,22 @@
+from flask_jwt_extended import get_jwt_identity
+
+from helpers.enum.user_role import UserRole
+from helpers.exceptions import CustomException
+from services.user import UserServices
+from utils.rbac import accessed_by
+
+
+@accessed_by(UserRole.ADMIN.value)
+class RemoveUserController:
+    def __init__(self, user_id, user_service=None):
+        self.performer_id = get_jwt_identity()
+        self.user_id = user_id
+        self.user_service = user_service or UserServices()
+
+    def __call__(self):
+        try:
+            self.user_service.remove_user(self.user_id)
+        except CustomException as custom_error:
+            return custom_error.dump(), custom_error.code
+        else:
+            return {"result": "removed"}, 200
--- src/services/user.py
+++ src/services/user.py
@@ -1,6 +1,15 @@
+from database import MysqlAccess, database
+from helpers.constants import SQLQueries
+from helpers.exceptions import DoNotExists
+
+
 class UserServices:
-    def add_user(self, username, password, role):
-        ...
+    def __init__(self, database_access=None):
+        self.database_access = database_access or MysqlAccess(database)
 
     def remove_user(self, user_id):
-        ...
+        with self.database_access as dao:
+            user = dao.read(SQLQueries.GET_USER_BY_ID, (user_id,), only_one=True)
+            if not user:
+                raise DoNotExists(f"user with user id {user_id} not found!")
+            dao.write(SQLQueries.REMOVE_USER, (user_id,))
